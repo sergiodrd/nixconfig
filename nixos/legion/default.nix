dfa641319b508b0459978af7e0edabbea82922eb
sergio <sergiod.roddj@gmail.com>
2023-08-11 18:18:37 -0400

    cleaned up legion default.nix

--- nixos/legion/default.nix
+++ nixos/legion/default.nix
@@ -40,15 +40,9 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "legion"; # Define your hostname.
-  #networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
   # Enable networking
   networking.networkmanager.enable = true;
+  networking.hostName = "legion"; 
 
   # Set your time zone.
   time.timeZone = "America/Puerto_Rico";
@@ -67,12 +61,6 @@
     LC_TELEPHONE = "es_PR.UTF-8";
     LC_TIME = "es_PR.UTF-8";
   };
-
-  # Configure keymap in X11
-  #services.xserver = {
-  #  layout = "us";
-  #  xkbVariant = "";
-  #};
 
   users.users.sergio = {
     isNormalUser = true;
@@ -105,23 +93,9 @@
   #   enableSSHSupport = true;
   # };
 
-  # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-   services.openssh.enable = true;
+  services.openssh.enable = true;
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.05"; # Did you read the comment?
-
+  system.stateVersion = "23.05"; 
 }
